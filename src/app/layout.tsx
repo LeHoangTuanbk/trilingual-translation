46c317091e7d18ed4aeb33e70281616c26b8cf21
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-09 22:05:57 +0900

    Update: Add make it natural markdown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 import { Providers } from "./providers";
+import { Header } from "@/components";
 
 export const metadata: Metadata = {
   title: "Trilingual translator",
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Header />
+        </Providers>
       </body>
     </html>
   );
--- src/app/make-it-natural/page.tsx
+++ src/app/make-it-natural/page.tsx
@@ -1,3 +1,42 @@
+import {
+  Box,
+  Heading,
+  Textarea,
+  Button,
+  VStack,
+  Select,
+  Text,
+  Checkbox,
+} from "@chakra-ui/react";
+import { Languages } from "@/utils";
 export default function MakeItNaturalPage() {
-  return <div>Make it natural</div>;
+  return (
+    <Box p={5}>
+      <form>
+        <VStack mb="4" gap="4" alignItems="flex-start" w="100%">
+          <Heading as="h1" size="lg">
+            Make it natural
+          </Heading>
+          <Text>
+            This tool is designed to help you make your text more natural like
+            native speakers writings.
+          </Text>
+          <Textarea placeholder="Enter your text here" height="3xs" />
+          <Select mr="4" w="fit-content" minW="300px">
+            {Object.values(Languages).map((language) => (
+              <option key={language} value={language}>
+                {language}
+              </option>
+            ))}
+          </Select>
+          <Text>Context of the text</Text>
+          <Textarea placeholder="Enter context here" height="100px" />
+          <Button type="submit">Make it natural</Button>
+          <Text>Result</Text>
+          <Checkbox>Auto copy</Checkbox>
+          <Textarea placeholder="Result" height="3xs" />
+        </VStack>
+      </form>
+    </Box>
+  );
 }
