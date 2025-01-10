97b205ec53e81cbaf0a6b548b9f20004342ebd78
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-10 19:08:03 +0900

    Update: Finish ui

--- src/app/make-it-natural/page.tsx
+++ src/app/make-it-natural/page.tsx
@@ -9,6 +9,9 @@ import {
   Select,
   Text,
   Checkbox,
+  FormControl,
+  FormLabel,
+  HStack,
 } from "@chakra-ui/react";
 import { Languages } from "@/utils";
 import { useEffect, useState } from "react";
@@ -29,24 +32,43 @@ export default function MakeItNaturalPage() {
           <Heading as="h1" size="lg">
             Make it natural
           </Heading>
-          <Text>
-            This tool is designed to help you make your text more natural like
-            native speakers writings.
-          </Text>
-          <Textarea placeholder="Enter your text here" height="3xs" />
-          <Select mr="4" w="fit-content" minW="300px">
-            {Object.values(Languages).map((language) => (
-              <option key={language} value={language}>
-                {language}
-              </option>
-            ))}
-          </Select>
-          <Text>Context of the text</Text>
-          <Textarea placeholder="Enter context here" height="100px" />
-          <Button type="submit">Make it natural</Button>
-          <Text>Result</Text>
-          <Checkbox>Auto copy</Checkbox>
-          <Textarea placeholder="Result" height="3xs" />
+          <Box>
+            <Text>
+              This tool is designed to make your text sound more natural, like
+              the writing of native speakers
+            </Text>
+            <Text fontSize="sm">
+              Paste or type and press Ctrl+Enter (or Cmd+Enter) to make it
+              natural
+            </Text>
+          </Box>
+
+          <FormControl>
+            <Textarea placeholder="Enter your text here" height="150px" />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Context of the text</FormLabel>
+            <Textarea placeholder="Enter context here" height="50px" />
+          </FormControl>
+          <HStack>
+            <Text>Your text is in</Text>
+            <Select mr="4" w="fit-content" minW="300px">
+              {Object.values(Languages).map((language) => (
+                <option key={language} value={language}>
+                  {language}
+                </option>
+              ))}
+            </Select>
+            <Button type="submit">Make it natural</Button>
+          </HStack>
+          <HStack gap="4">
+            <Text>Result</Text>
+            <Checkbox>Auto copy</Checkbox>
+          </HStack>
+          <FormControl>
+            <Textarea placeholder="Result" height="3xs" />
+          </FormControl>
         </VStack>
       </form>
     </Box>
