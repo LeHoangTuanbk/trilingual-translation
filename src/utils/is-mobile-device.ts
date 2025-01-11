a5a0ec8a728eff5aa641773d1ada9546d43209c5
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-11 19:06:16 +0900

    Update: Fix

--- src/utils/is-mobile-device.ts
+++ src/utils/is-mobile-device.ts
@@ -1,4 +1,5 @@
 export const isMobileDevice = () => {
+  if (typeof window === "undefined" || !navigator) return false;
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
   );
