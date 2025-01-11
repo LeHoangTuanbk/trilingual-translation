8b670e2846ddfd703474c0519c297a1a24b548ec
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-11 12:07:36 +0900

    Update: Set min, max

--- src/features/make-it-natural/api/zod-schema.ts
+++ src/features/make-it-natural/api/zod-schema.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 import { Languages, MODELS } from "@/utils";
 export const makeItNaturalSchema = z.object({
-  text: z.string().min(1, "Text is required"),
+  text: z
+    .string()
+    .min(5, "The app performs better with 5 or more words.")
+    .max(1000, "The app performs better with 1000 or less words."),
   context: z.string(),
   language: z.enum(Object.values(Languages) as [string, ...string[]]),
   selectedModel: z.enum(MODELS as unknown as [string, ...string[]]),
