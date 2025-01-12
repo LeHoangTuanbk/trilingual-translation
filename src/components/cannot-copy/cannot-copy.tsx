97a07f981479b2260ee1d7741901b2f63cfe5caa
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-12 15:16:26 +0900

    Update: Check mobile

--- src/components/cannot-copy/cannot-copy.tsx
+++ src/components/cannot-copy/cannot-copy.tsx
@@ -1,11 +1,19 @@
-import { Text, Hide } from "@chakra-ui/react";
+"use client";
+import { Text, Box } from "@chakra-ui/react";
+import { isMobileDevice } from "@/utils";
+import { useState, useEffect } from "react";
 export const CannotCopy = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(!!isMobileDevice());
+  }, []);
   return (
-    <Hide above="md">
+    <Box display={isMobile ? "block" : "none"}>
       <Text fontSize="sm">
         ※On some mobile devices, the auto copy function may not work. You need
         to copy the text manually.
       </Text>
-    </Hide>
+    </Box>
   );
 };
--- src/utils/is-mobile-device.ts
+++ src/utils/is-mobile-device.ts
@@ -1,6 +1,10 @@
 export const isMobileDevice = () => {
   if (typeof window === "undefined" || !navigator) return false;
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
+  const userAgent = navigator.userAgent.toLowerCase();
+  return (
+    /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+      userAgent
+    ) ||
+    (navigator.maxTouchPoints && navigator.maxTouchPoints > 2)
   );
 };
