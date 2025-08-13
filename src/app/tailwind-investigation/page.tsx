c2bdd7a4e8061cfc043217103add263172b53bed
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-08-13 16:45:11 +0900

    Add: tailwind

---
+++ src/app/tailwind-investigation/page.tsx
@@ -0,0 +1,9 @@
+const TailwindCssInvestigation = () => {
+  return (
+    <div className="text-xl font-bold text-blue-900 underline bg-red-200">
+      Hello
+    </div>
+  );
+};
+
+export default TailwindCssInvestigation;
